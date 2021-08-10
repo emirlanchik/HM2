ef06b8240c7acbdc256514153ce74c4c95ba5ec2
emirlanchik <88429362+emirlanchik@users.noreply.github.com>
2021-08-10 18:17:27 +0300

    Add files via upload

---
+++ hw 2_1.py
@@ -0,0 +1,14 @@
+name = input("What's your name? ").title()
+surname = input("What's your surname? ").title()
+country = input("Where you from? ")
+medal = 'serebro'
+if medal == 'zoloto':
+    print('Go')
+if medal == 'serebro':
+    print('stop')
+elif medal == 'bronza':
+    print('go')
+else:
+    'look at situation'
+
+
